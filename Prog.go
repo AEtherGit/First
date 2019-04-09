f455a0f80138c78ef049a019b031294c1534f913
AEtherGit <skynet40000@mail.ru>
2019-04-09 15:01:52 +0300

    l

--- Prog.go
+++ Prog.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+  "fmt"
+  "net/http"
+  "io/ioutil"
+)
 
 func main() {
-    fmt.Println("Hello")
+  resp, _ := http.Get("https://mvideo.ru")
+  bytes, _ := ioutil.ReadAll(resp.Body)
+  string_body := string(bytes)
+  fmt.Println(string_body)
+  resp.Body.Close()
 }
