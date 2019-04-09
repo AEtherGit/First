b43977e583a185a8e7cbbf1908b22e803ae215bf
AEtherGit <skynet40000@mail.ru>
2019-04-09 21:25:13 +0300

    dsa

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+  "fmt"
+  "strings"
+  "net/http"
+  "io/ioutil"
+)
+
+func main() {
+  resp, err := http.Get("https://mvideo.ru")
+  if err != nil {
+    log.Fatal(err)
+  }
+  bytes, err := ioutil.ReadAll(resp.Body)
+  if err != nil {
+    log.Fatal(err)
+  }
+  stringBody := string(bytes)
+  mass := strings.Fields(stringBody)
+  for i, v := range mass {
+    if v == "<a" {
+      fmt.Println(mass[i])
+    }
+  }
+//  fmt.Println(string_body)
+  resp.Body.Close()
+}
