692a4750ff3a41a022a684ecfed1c5aa5926357d
AEtherGit <skynet40000@mail.ru>
2019-04-09 21:33:02 +0300

    fas

--- main.go
+++ main.go
@@ -1,28 +1,28 @@
 package main
 
 import (
-  "fmt"
-  "strings"
-  "net/http"
-  "io/ioutil"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
 )
 
 func main() {
-  resp, err := http.Get("https://mvideo.ru")
-  if err != nil {
-    log.Fatal(err)
-  }
-  bytes, err := ioutil.ReadAll(resp.Body)
-  if err != nil {
-    log.Fatal(err)
-  }
-  stringBody := string(bytes)
-  mass := strings.Fields(stringBody)
-  for i, v := range mass {
-    if v == "<a" {
-      fmt.Println(mass[i])
-    }
-  }
-//  fmt.Println(string_body)
-  resp.Body.Close()
+	resp, err := http.Get("https://mvideo.ru")
+	if err != nil {
+		log.Fatal(err)
+	}
+	bytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	stringBody := string(bytes)
+	mass := strings.Fields(stringBody)
+	for i, v := range mass {
+		if v == "<a" {
+			fmt.Println(mass[i])
+		}
+	}
+	//  fmt.Println(string_body)
+	resp.Body.Close()
 }
